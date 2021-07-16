1b1756a4edffae3576e9f62b3ef2a16dd162b969
marson parulian <marson.parulian@gmail.com>
2021-07-16 18:37:31 +1000

    <InputFOrmPanel /> - init tests

--- src/components/organisms/InputFormPanel/InputFormPanel.js
+++ src/components/organisms/InputFormPanel/InputFormPanel.js
@@ -1,11 +1,19 @@
 import React from "react";
+import propTypes from "prop-types";
+import InputForm from "../InputForm/InputForm";
 
 /**
- * Contain all components related to input handling
+ * Contain all components related to input handling.
+ * This is where most of the logic happen.
  */
 class InputFormPanel extends React.Component {
     render() {
-        <div>Start</div>
+        return (
+            <div id="input-form-panel">
+                <InputForm
+                />
+            </div>
+        )
     }
 }
 

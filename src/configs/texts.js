4fa74b5f35601d3a8f3f95f2c62baaef9c67af9b
marson parulian <marson.parulian@gmail.com>
2021-07-22 22:33:32 +1000

    <ResultAge /> - add text messages

--- src/configs/texts.js
+++ src/configs/texts.js
@@ -4,5 +4,9 @@ const texts = {
     NAME_INPUT_PLACEHOLDER: "Single word name..",
     SUBMIT_BUTTON_TEXT: "Analyze",
     RESULT_PANEL_DEFAULT_MSG: "Type a name above to be analyzed",
+    // Text messages for result by age
+    RESULT_AGE_BUSY_TEXT: "Predicting age..",
+    RESULT_AGE_OK_PRE_TEXT: "Predicted age : ",
+    RESULT_AGE_ERROR_TEXT: "Error. Unable to predict the age.",
 }
 export default texts;

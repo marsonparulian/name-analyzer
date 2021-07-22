a80ddc33e6474e05812bb1b31a374095f3adc019
marson parulian <marson.parulian@gmail.com>
2021-07-22 23:12:44 +1000

    <ResultAgeContainer /> - register to store

--- src/components/store.js
+++ src/components/store.js
@@ -5,21 +5,24 @@ import reduxThunk from "redux-thunk";
 // Import Reducers
 import inputFormReducer from "./organisms/InputForm/inputFormReducer";
 import resultPanelReducer from "./organisms/ResultPanel/resultPanelReducer"
+import resultAgeReducer from "./molecules/ResultAge/resultAgeReducer";
 
 // Import default state
 import inputFormDefault from "./organisms/InputForm/inputFormDefault";
 import resultPanelDefault from "./organisms/ResultPanel/resultPanelDefault";
+import resultAgeDefault from "./molecules/ResultAge/resultAgeDefault";
 
 // Root reducer
 export const rootReducer = combineReducers({
     inputForm: inputFormReducer,
     resultPanel: resultPanelReducer,
+    resultAge: resultAgeReducer,
 });
-
 // Default root state
 export const defaultRootState = {
     inputForm: inputFormDefault,
     resultPanel: resultPanelDefault,
+    resultAge, resultAgeDefault,
 };
 // Create store
 const store = createStore(rootReducer, defaultRootState, applyMiddleware(reduxThunk));

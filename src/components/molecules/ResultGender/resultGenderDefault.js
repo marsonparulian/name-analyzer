8faf3ade321f2131295b7603358f9f1f289e79df
marson parulian <marson.parulian@gmail.com>
2021-07-24 20:39:53 +1000

    <ResultGender /> - quick fix

--- src/components/molecules/ResultGender/resultGenderDefault.js
+++ src/components/molecules/ResultGender/resultGenderDefault.js
@@ -1,6 +1,6 @@
 // Default state form <ResultGender />
 
-export default const defaultState = {
+export default defaultState = {
     // Possible values: `ok`, `error`.
     status: 'ok',
     // If not falsy, will show this message. This message is used to show 'busy' or 'error' message

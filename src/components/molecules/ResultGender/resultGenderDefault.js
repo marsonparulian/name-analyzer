afcf0424378b0fcd028ad4499e7ce5e196833ce3
marson parulian <marson.parulian@gmail.com>
2021-07-24 19:18:01 +1000

    <ResultGEnder /> - default state

---
+++ src/components/molecules/ResultGender/resultGenderDefault.js
@@ -0,0 +1,10 @@
+// Default state form <ResultGender />
+
+export default const defaultState = {
+    // Possible values: `ok`, `error`.
+    status: 'ok',
+    // If not falsy, will show this message. This message is used to show 'busy' or 'error' message
+    msg: "",
+    // The predicted gender
+    gender: "",
+}
